98a18f7c430ea216229d82019a876cfc69dd0445
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-12-14 09:40:29 -0300

    Create a utils folder

---
+++ src/utils/ListItems.ts
@@ -0,0 +1,18 @@
+export const FirstSectionListItems = [
+  {
+    id: 1,
+    text: "No sabes por donde comenzar y entender las diferentes técnicas y herramientas en el momento de aplicarlas a tus dibujos.",
+  },
+  {
+    id: 2,
+    text: "Si quieres llevar tus trabajos a un alto nivel de detalles en un lapso de tiempo corto.",
+  },
+  {
+    id: 3,
+    text: "Te apasiona el detalle y la precisión en tus dibujos. ",
+  },
+  {
+    id: 4,
+    text: "Estás practicando siempre y no ves avances empíricos.",
+  },
+];
