a73a934b000fd33315bc7765bfcd4fe1cb031961
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-12-18 18:34:43 -0300

    Create a utility file containing the course modules

---
+++ src/utils/CourseModules.ts
@@ -0,0 +1,147 @@
+import type { ModuleCourseProps } from "../types/Props";
+
+export const courseModules: ModuleCourseProps[] = [
+  {
+    moduleNumber: "Módulo 1",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Introducción al Realismo",
+      },
+      {
+        class: "Aula 2",
+        description: "Mentalidad correcta",
+      },
+      {
+        class: "Aula 3",
+        description: "Materiales",
+      },
+      {
+        class: "Aula 4",
+        description: "Nociones de Luz y Sombra (volumen y profundidad) ",
+      },
+      {
+        class: "Aula 5",
+        description: "Ejercicio básico (Transición de tonos)",
+      },
+      {
+        class: "Aula 6",
+        description: "Ejercicio básico (Esfera)",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 2",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Técnica de Boceto (Cuadrícula)",
+      },
+      {
+        class: "Aula 2",
+        description: "Técnica de Líneas de Apoyo",
+      },
+      {
+        class: "Aula 3",
+        description: "Técnica de boceto (calque)",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 3",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Ejercicio de Tela",
+      },
+      {
+        class: "Aula 2",
+        description: "Ejercicio Pieza de ajedrez (Cromado)",
+      },
+      {
+        class: "Aula 3",
+        description: "Ejercicio Gotas (Transparencia)",
+      },
+      {
+        class: "Aula 4",
+        description: "Ejercicio Pera realista",
+      },
+      {
+        class: "Aula 5",
+        description: "Madera (textura)",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 4",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Narcies",
+      },
+      {
+        class: "Aula 2",
+        description: "Bocas",
+      },
+      {
+        class: "Aula 3",
+        description: "Ojos",
+      },
+      {
+        class: "Aula 4",
+        description: "Orejas",
+      },
+      {
+        class: "Aula 5",
+        description: "Rostro Completo",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 5",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Cabello Masculino corto",
+      },
+      {
+        class: "Aula 2",
+        description: "Cabello Largo Dama",
+      },
+      {
+        class: "Aula 3",
+        description: "Cabello rasta",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 6",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Rostro Bebé",
+      },
+      {
+        class: "Aula 2",
+        description: "Rostro Fememnino",
+      },
+      {
+        class: "Aula 3",
+        description: "Rostro Masculino",
+      },
+    ],
+  },
+  {
+    moduleNumber: "Módulo 7",
+    classes: [
+      {
+        class: "Aula 1",
+        description: "Rostro Mojado",
+      },
+      {
+        class: "Aula 2",
+        description: "Rostro Anciano",
+      },
+    ],
+  },
+];
