a97ed0f2e6e119bd1bcb970237fafc3d44a7bfac
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-12-18 18:33:40 -0300

    Create a qas utility file

---
+++ src/utils/QAs.ts
@@ -0,0 +1,27 @@
+export const qas = [
+  {
+    question: "¿Tengo soporte en mis prácticas?",
+    answer:
+      "Claro, dentro del grupo de estudiantes tendrás respaldo, direccionamiento, para dudas.",
+  },
+  {
+    question: "¿Qué necesitaría para comenzar?",
+    answer:
+      "Puedes comenzar con materiales sencillos en un comienzo. Como un bloc de papeples de dibujo, lápices (B, 3B y 5B), y una goma de borrar.",
+  },
+  {
+    question: "¿Funciona para niños?",
+    answer:
+      "En verdad, al ser trabajos demorados y complejos, para un niño sería difícil mantenerlos motivados. Aconsejo para ellos trabajos más sueltos y libres de expresión.",
+  },
+  {
+    question: "¿Cómo saber si estoy evolucionando?",
+    answer:
+      "Esto dependerá de tu propio camino creado desde el comienzo, cuando veas un progreso comparando con tu anterior ejercicio.",
+  },
+  {
+    question: "¿Se aprenderá Anatomía, Perspectiva, etc.?",
+    answer:
+      "Uno de los fundamentos es el desarrollo técnico del Realismo. Es decir, un estudio que desarrollará tu percepción a partir de  una imagen de referencia para conseguir un resultado cercano a lo real. Y como consecuencia, aprenderá más que eso. Pero no, no enseño estudio de Anatomía, Perspectiva, etc.",
+  },
+];
